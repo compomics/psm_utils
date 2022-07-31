77d73243cdec9c33eae60ce54e64b415309b2a61
RalfG <ralfg@hotmail.be>
2022-07-31 13:29:10 +0200

    Add imports in __init__ for easier import from psm_utils: Allows `from psm_utils import Peptidoform, PeptideSpectrumMatch, PSMList` instead of ``` from psm_utils.peptidoform import Peptidoform from psm_utils.psm import PeptideSpectrumMatch from psm_utils.psm_list import PSMList ```
    Add __len__ method to PSMList

--- psm_utils/__init__.py
+++ psm_utils/__init__.py
@@ -1,3 +1,7 @@
-"""Common functionality for handling peptidoforms, PSMs and PSM lists."""
+"""Common utilities for parsing and handling PSMs and search engine results."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.0.dev0"
+
+from psm_utils.peptidoform import Peptidoform
+from psm_utils.psm import PeptideSpectrumMatch
+from psm_utils.psm_list import PSMList

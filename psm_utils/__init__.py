dd4c439da18c149cc40b22d44c4ee6b37117c5c8
RalfG <ralfg@hotmail.be>
2022-09-02 17:38:52 +0200

    bump dev version

--- psm_utils/__init__.py
+++ psm_utils/__init__.py
@@ -1,6 +1,6 @@
 """Common utilities for parsing and handling PSMs, and search engine results."""
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0.dev1"
 
 from psm_utils.peptidoform import Peptidoform
 from psm_utils.psm import PeptideSpectrumMatch

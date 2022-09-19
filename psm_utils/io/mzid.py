6902dd92c21de7cf3882dc819b1ade6f253690cf
ArthurDeclercq <arre_declercq@hotmail.com>
2022-09-19 16:04:37 +0200

    implement mzid writer

--- psm_utils/io/mzid.py
+++ psm_utils/io/mzid.py
@@ -4,8 +4,15 @@ import logging
 import re
 import xml.etree.ElementTree as ET
 from pathlib import Path
-from typing import Union
+from typing import Optional, Union
 
+from pyopenms import (
+    AASequence,
+    MzIdentMLFile,
+    PeptideEvidence,
+    PeptideHit,
+    PeptideIdentification,
+)
 from pyteomics import mzid
 
 from psm_utils.io._base_classes import ReaderBase, WriterBase
@@ -95,7 +102,8 @@ class MzidReader(ReaderBase):
                 raw_file = Path(spectrum["location"]).stem
                 for entry in spectrum["SpectrumIdentificationItem"]:
                     psm = self._get_peptide_spectrum_match(
-                        spectrum_title, raw_file, entry)
+                        spectrum_title, raw_file, entry
+                    )
                     yield psm
 
     def read_file(self) -> PSMList:
@@ -185,12 +193,14 @@ class MzidReader(ReaderBase):
             "length": len(sii["PeptideSequence"]),
         }
         if self._source == "MS-GF+":
-            metadata.update({
-                "MS-GF:DeNovoScore": sii["MS-GF:DeNovoScore"],
-                "MS-GF:EValue": sii["MS-GF:EValue"],
-                "MS-GF:DeNovoScore": sii["MS-GF:DeNovoScore"],
-                "IsotopeError": sii["IsotopeError"],
-            })
+            metadata.update(
+                {
+                    "MS-GF:DeNovoScore": sii["MS-GF:DeNovoScore"],
+                    "MS-GF:EValue": sii["MS-GF:EValue"],
+                    "MS-GF:DeNovoScore": sii["MS-GF:DeNovoScore"],
+                    "IsotopeError": sii["IsotopeError"],
+                }
+            )
         return metadata
 
     def _get_peptide_spectrum_match(
@@ -233,18 +243,128 @@ class MzidReader(ReaderBase):
 
 
 class MzidWriter(WriterBase):
-    """Writer for MaxQuant msms.txt files (not implemented)."""
+    """Writer for MzIdentMl files."""
 
-    def __init__(self, filename):
-        raise NotImplementedError()
+    def __init__(
+        self,
+        filename: Union[str, Path],
+        example_psm: Optional[PeptideSpectrumMatch] = None,
+        *args,
+        **kwargs,
+    ):
+        """
+        Writer for psm_utils MZID format.
+
+        Parameters
+        ----------
+        filename: str, Pathlib.Path
+            Path to PSM file.
+        example_psm: psm_utils.psm.PeptideSpectrumMatch, optional
+            Example PSM, required to extract the column names when writing to a new
+            file. Should contain all fields that are to be written to the PSM file,
+            i.e., all items in the :py:attr:`provenance_data`, :py:attr:`metadata`, and
+            :py:attr:`rescoring_features` attributes. In other words, items that are
+            not present in the example PSM will not be written to the file, even though
+            they are present in other PSMs passed to :py:meth:`write_psm` or
+            :py:meth:`write_file`.
+        """
+        super().__init__(filename, *args, **kwargs)
+
+        self._writer = None
+        self._peptide_ids = None
+        self._example_psm = example_psm  # Use for writing general information to mzid?
+        # self._protein_ids = None  TODO implement protein ids with fasta file
+
+    def __enter__(self) -> MzidWriter:
+        # TODO
+        self._writer = MzIdentMLFile()
+        self._peptide_ids = {}
+
+    def __exit__(self, *args, **kwargs) -> None:
+        self._writer.store(
+            self.filename, [], list(self._peptide_ids.values())
+        )  # Empty list is for protein ids
+        self._peptide_ids = None
 
     def write_psm(self, psm: PeptideSpectrumMatch):
-        """Write a single PSM to the MaxQuant msms.txt PSM file."""
-        raise NotImplementedError()
+        """Write a single PSM to a Peptidehit object."""
+
+        # Get peptide id from dict or create new peptide id if no peptide id exists for this spectrum
+        if psm["spectrum_id"] in self._peptide_ids.keys():
+            peptide_id = self._peptide_ids[psm["spectrum_id"]]
+        else:
+            peptide_id = PeptideIdentification()
+            peptide_id.setRT(psm["retention_time"])
+            peptide_id.setMZ(psm["precursor_mz"])
+            peptide_id.setScoreType(psm["source"])  # replace with psm_list object
+            peptide_id.setHigherScoreBetter(True)  # replace with psm_list object
+
+            self._peptide_ids[psm["spectrum_id"]] = peptide_id
+
+        # Create new peptide hit
+        peptide_hit = PeptideHit()
+        peptide_hit.setScore(psm["score"])
+        peptide_hit.setRank(int(psm["Rank"]))  # has to be int
+        peptide_hit.setCharge(psm["peptide"].precursor_charge)
+
+        # add metadata
+        for metadata_key in psm["metadata"].keys():
+            peptide_hit.setMetaValue(metadata_key, psm["metadata"][metadata_key])
+
+        for rescoring_key in psm["rescoring_features"].keys():
+            peptide_hit.setMetaValue(
+                metadata_key, psm["rescoring_features"][rescoring_key]
+            )
+
+        # add sequece to hit
+        peptide = self._parse_proforma_sequence(psm["peptide"].proforma)
+        peptide_hit.setSequence(AASequence.fromString(peptide))
+
+        # Add peptide evidence(s)
+        evidences = []
+        for protein_accession in psm["protein_list"]:
+            ev = PeptideEvidence()
+            ev.setProteinAccession(protein_accession)
+            evidences.append(ev)
+
+        peptide_hit.setPeptideEvidences(evidences)
+        peptide_id.insertHit(peptide_hit)
 
     def write_file(self, psm_list: PSMList):
-        """Write entire PSMList to the MaxQuant msms.txt PSM file."""
-        raise NotImplementedError()
+        """Write entire PSMList to mzid file."""
+
+        mzid_file = MzIdentMLFile()
+        for psm in psm_list:
+            self.write_psm(psm)
+
+        mzid_file.store(self.filename, [], list(self._peptide_ids.values()))
+
+    @staticmethod
+    def _parse_proforma_sequence(seq):
+        "Parse proforma sequence to mzid input sequence"
+        charge_pattern = re.compile(r"(\/[\d]+)$")
+        seq = charge_pattern.sub("", seq)
+
+        proforma_pattern = re.compile(
+            r"^(?P<n_term>\[[^-]+\])?(-)?(?P<sequence>[A-z]+)(-)?(?P<c_term>\[.+\])?"
+        )
+        matches = proforma_pattern.match(seq)
+
+        mzid_seq = "".join(
+            filter(
+                None,
+                [
+                    ".",
+                    matches.group("n_term"),
+                    matches.group("sequence"),
+                    ".",
+                    matches.group("c_term"),
+                ],
+            )
+        )
+
+        # TODO check if all square brackets should be change to round ones
+        return mzid_seq.replace("[", "(").replace("]", ")")
 
 
 class UnknownMzidScore(PSMUtilsIOException):

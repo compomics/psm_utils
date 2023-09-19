02cc6ef6d487f38d875242c49e9da44afcf95c76
RalfG <ralfg@hotmail.be>
2023-09-19 19:18:35 +0200

    peptidoform: Add iter and len methods

--- tests/test_peptidoform.py
+++ tests/test_peptidoform.py
@@ -1,7 +1,33 @@
+from pyteomics import proforma
+
 from psm_utils.peptidoform import Peptidoform
 
 
 class TestPeptidoform:
+
+    def test__len__(self):
+        test_cases = [
+            ("ACDEFGHIK", 9),
+            ("[ac]-AC[cm]DEFGHIK", 9),
+            ("[ac]-AC[Carbamidomethyl]DEFGHIK", 9),
+            ("[Acetyl]-AC[cm]DEFGK", 7),
+            ("<[cm]@C>[Acetyl]-ACDK", 4),
+            ("<[Carbamidomethyl]@C>[ac]-ACDEFGHIK", 9),
+        ]
+
+        for test_case_in, expected_out in test_cases:
+            peptidoform = Peptidoform(test_case_in)
+            assert len(peptidoform) == expected_out
+
+    def test__iter__(self):
+        for aa, mods in Peptidoform("ACDEM[U:35]K"):
+            assert isinstance(aa, str)
+            if mods is not None:
+                assert isinstance(mods, list)
+                for mod in mods:
+                    assert isinstance(mod, proforma.TagBase)
+
+
     def test_rename_modifications(self):
         label_mapping = {"ac": "Acetyl", "cm": "Carbamidomethyl"}
 

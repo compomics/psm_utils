57d94eced9230ca090fce53869bd6903b9f0d95c
jonasscheid <jonas.scheid@uni-tuebingen.de>
2023-10-04 16:16:46 +0000

    draft of additional pyopenms reader

--- psm_utils/io/idxml.py
+++ psm_utils/io/idxml.py
@@ -13,9 +13,13 @@ Notes
 
 from __future__ import annotations
 
+from abc import abstractmethod
 import re
 
-from pyteomics.openms import idxml
+try:
+    import pyopenms as oms
+except ImportError:
+    from pyteomics.openms import idxml
 
 from psm_utils.io._base_classes import ReaderBase
 from psm_utils.psm import PSM
@@ -26,20 +30,19 @@ MOD_PATTERN = re.compile(r"\(((?:[^)(]+|\((?:[^)(]+|\([^)(]*\))*\))*)\)")
 MOD_PATTERN_NTERM = re.compile(r"^\.\[((?:[^][]+|\[(?:[^][]+|\[[^][]*\])*\])*)\]")
 MOD_PATTERN_CTERM = re.compile(r"\.\[((?:[^][]+|\[(?:[^][]+|\[[^][]*\])*\])*)\]$")
 
-
-class IdXMLReader(ReaderBase):
+class AIdXMLReader(ReaderBase):
+    @abstractmethod
     def __iter__(self):
-        """Iterate over file and return PSMs one-by-one."""
-        with idxml.read(str(self.filename)) as reader:
-            for entry in reader:
-                for peptide_hit in entry["PeptideHit"]:
-                    yield self._parse_psm(entry, peptide_hit)
-
+        raise NotImplementedError()
+    
+    @abstractmethod
+    def _parse_psm(self):
+        raise NotImplementedError()
+    
     @staticmethod
     def _parse_peptidoform(sequence: str, charge: int):
         """
         Parse idXML peptide to :py:class:`~psm_utils.peptidoform.Peptidoform`.
-
         Notes
         -----
         Implemented according to the documentation on
@@ -56,23 +59,21 @@ class IdXMLReader(ReaderBase):
         sequence += f"/{charge}"
         return sequence
 
-    @staticmethod
-    def _parse_is_decoy(target_decoy: str):
-        if target_decoy == "target":
-            return False
-        elif target_decoy == "decoy":
-            return True
-        elif target_decoy == "target+decoy":
-            return False
-        else:
-            return None
+
+class IdXMLReader(AIdXMLReader):
+    def __iter__(self):
+        """Iterate over file and return PSMs one-by-one."""
+        with idxml.read(str(self.filename)) as reader:
+            for entry in reader:
+                for peptide_hit in entry["PeptideHit"]:
+                    yield self._parse_psm(entry, peptide_hit)
 
     def _parse_psm(self, entry: dict, peptide_hit: dict) -> PSM:
         """Parse idXML PSM to :py:class:`~psm_utils.psm.PSM`."""
         return PSM(
             peptidoform=self._parse_peptidoform(peptide_hit["sequence"], peptide_hit["charge"]),
             spectrum_id=entry["spectrum_reference"],
-            is_decoy=self._parse_is_decoy(peptide_hit["target_decoy"]),
+            is_decoy= peptide_hit["target_decoy"] == "decoy",
             score=peptide_hit["score"],
             precursor_mz=entry["MZ"],
             retention_time=entry["RT"],
@@ -83,4 +84,41 @@ class IdXMLReader(ReaderBase):
                 "idxml:higher_score_better": str(entry["higher_score_better"]),
                 "idxml:significance_threshold": str(entry["significance_threshold"]),
             },
+        )
+
+
+class OmsIdXMLReader(AIdXMLReader):
+    def __iter__(self):
+        """Iterate over file and return PSMs one-by-one."""
+        # TODO: This is a bit hacky, is there a cleaner way to do this?
+        try:
+            protein_ids, peptide_ids = [], []
+            oms.IdXMLFile().load(str(self.filename), protein_ids, peptide_ids)
+        except NameError:
+            raise ModuleNotFoundError("Install pyopenms to use OmsIdXMLReader.")
+        for peptide_id in peptide_ids:
+            for peptide_hit in peptide_id.getHits():
+                yield self._parse_psm(peptide_id, peptide_hit)
+
+    def _parse_psm(self, peptide_id: oms.PeptideIdentification, peptide_hit: oms.PeptideHit) -> PSM:
+        """Parse idXML PSM to :py:class:`~psm_utils.psm.PSM` using pyopenms"""
+        return PSM(
+            peptidoform = self._parse_peptidoform(peptide_hit.getSequence().toString(), peptide_hit.getCharge()),
+            spectrum_id = peptide_id.getMetaValue("spectrum_reference"),
+            is_decoy = peptide_hit.getMetaValue("target_decoy") == "decoy",
+            score = peptide_hit.getScore(),
+            precursor_mz = peptide_id.getMZ(),
+            retention_time = peptide_id.getRT(),
+            # TODO: ion mobility is currently not parsed to idXML so this needs to be read out from mzml
+            protein_list=[accession.decode() for accession in peptide_hit.extractProteinAccessionsSet()],
+            rank = peptide_hit.getRank() + 1, # 0-based to 1-based
+            source="idXML",
+            # TODO: Store all peptide hit related metadata?
+            metadata = {
+                "idxml:score_type": str(peptide_id.getScoreType()),
+                "idxml:higher_score_better": str(peptide_id.isHigherScoreBetter()),
+                "idxml:significance_threshold": str(peptide_id.getSignificanceThreshold())
+            }
+            # TODO: provenance_data
+            # TODO: rescoring_features (Is there a fixed nomenclature for using these?)
         )

f6e4086b9c5418acaf41ef08f231be5250cac81c
RalfG <ralfg@hotmail.be>
2024-08-14 14:38:11 +0200

    TSV: Raise exception if three consecutive rows cannot be parsed

--- tests/test_io/test_tsv.py
+++ tests/test_io/test_tsv.py
@@ -1,5 +1,8 @@
 """Tests for psm_utils.io.tsv."""
 
+import pytest
+
+from psm_utils.io.exceptions import PSMUtilsIOException  # noqa: F401
 from psm_utils.io.tsv import TSVReader, TSVWriter  # noqa: F401
 
 test_cases = [
@@ -30,3 +33,19 @@ class TestTSVReader:
     def test__parse_entry(self):
         for test_in, expected_out in test_cases:
             assert TSVReader._parse_entry(test_in) == expected_out
+
+    def test_iter(self):
+        reader = TSVReader("tests/test_data/test.tsv")
+        for psm in reader:
+            assert psm.peptidoform == "ACDEK/2"
+            assert psm.spectrum_id == "peptide1"
+            assert psm.provenance_data == {}
+            assert psm.metadata == {}
+            assert psm.rescoring_features == {}
+            break
+
+    def test_iter_raises(self):
+        with TSVReader("tests/test_data/peprec.tsv") as reader:
+            with pytest.raises(PSMUtilsIOException):
+                for psm in reader:
+                    pass

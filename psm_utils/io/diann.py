2a7b56e1efb2f69e2212ba49eba6f11e22d4d03d
rodvrees <robbe.devreese@hotmail.com>
2024-10-31 15:46:35 +0100

    formatting

--- psm_utils/io/diann.py
+++ psm_utils/io/diann.py
@@ -60,7 +60,7 @@ class DIANNReader(ReaderBase, ABC):
             peptidoform=self._parse_peptidoform(
                 psm_dict["Modified.Sequence"], psm_dict["Precursor.Charge"]
             ),
-            spectrum_id=psm_dict["MS2.Scan"], 
+            spectrum_id=psm_dict["MS2.Scan"],
             run=psm_dict["Run"],
             is_decoy=False,
             qvalue=psm_dict[

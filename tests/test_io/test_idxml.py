cba05bf60fee8a229a8b9f30e80f3a2085646e6c
singjc <justincsing@gmail.com>
2025-01-23 10:23:16 -0500

    update,tests: update sage and idxml tests
    - add ion mobility and predicted filed to test_psm obj
    - update expected hash in idxml test for outputed sage idxml

--- tests/test_io/test_idxml.py
+++ tests/test_io/test_idxml.py
@@ -107,7 +107,7 @@ class TestIdXMLWriter:
         assert sha == expected_sha
 
     def test_write_file_without_pyopenms_objects(self):
-        expected_sha = "148889926276fbe391e23ed7952c3a8410fc67ffb099bbf1a72df75f8d727ccd"
+        expected_sha = "3b227edf1649021a01feb706e3b3effb939d4fdeccb4cd7111be30bb2e6280e7"
         reader = SageTSVReader("./tests/test_data/results.sage.tsv")
         psm_list = reader.read_file()
         writer = IdXMLWriter("./tests/test_data/test_out_sage.idXML")

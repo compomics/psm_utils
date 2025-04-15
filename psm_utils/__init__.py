95e82181c3d7e5f6fcd61832f3cc76134df4c92e
Ralf Gabriels <ralfg@hotmail.be>
2025-04-15 18:11:54 +0200

    Version bump

--- psm_utils/__init__.py
+++ psm_utils/__init__.py
@@ -1,6 +1,6 @@
 """Common utilities for parsing and handling PSMs, and search engine results."""
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __all__ = ["Peptidoform", "PSM", "PSMList"]
 
 from warnings import filterwarnings
